195ab5ce510b1b5c4db90fc083b7d7dca0228f08
iit-image-kawada <maronhiro@ezweb.ne.jp>
2022-02-10 20:46:39 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -129,18 +129,14 @@
 
 	/* 演習1 ここから */
 
-	function sub() {}
+	function sub(){eqlNum.value = parseFloat(midNum[0].value) - parseFloat(midNum[1].value}
 
-	function add(){}
+	function add(){eqlNum.value = parseFloat(midNum[0].value) + parseFloat(midNum[1].value}
 
 	/* 演習1 ここまで */
 
 	/* TODO: 演習2 以降は下記に自由に記入 */
-
 	
-	
-	function sub(){
-		eqlNum.value = parseFloat(midNum[0].value) - parseFloat(midNum[1].value);
 	}
 
 	/* 演習の編集範囲 おわり */
